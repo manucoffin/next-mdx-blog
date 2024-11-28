7fd73803ea0cbdd4877be959ad8af68142ead30f
manucoffin <manuel.coffin@gmail.com>
2024-11-28 18:28:15 +0700

    auto commit

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,9 @@
 import type { MDXComponents } from "mdx/types";
+import { CTACard } from "./app/blog/_components/cta-card";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    CTACard,
   };
 }
